9adaf98b41554978df80c01fc1ea272a2d3e7f10
Viktor Mileski <145224964+viksamiles77@users.noreply.github.com>
2024-06-05 20:48:57 +0200

    Controllers
    Class_01 / Controllers

---
+++ Class_01/Demo_App_1/Demo_App_1/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_App_1.Controllers
+{
+    public class UserController : Controller
+    {
+        // You need to create new view for new controller
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult PlainText()
+        {
+            return Content("This is plain text!");
+        }
+
+        public IActionResult JsonAction()
+        {
+            List<KeyValuePair<int, string>> names = new List<KeyValuePair<int, string>> {
+                new KeyValuePair<int, string> ( 1, "Risto"),
+                new KeyValuePair<int, string> (2, "Slave")
+            };
+            return Json(names);
+        }
+    }
+}
