2dd87f271ac637e8cd3cb9d965d47f4e21316cbb
Viktor Mileski <145224964+viksamiles77@users.noreply.github.com>
2024-06-07 18:58:08 +0200

    Routing/Controller added

---
+++ Class_02/Demo_App_1/Demo_App_1/Controllers/InvoiceController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_App_1.Controllers
+{
+    [Route("fakturi/[Action]")]
+    public class InvoiceController : Controller
+    {
+        //[Route("fakturi/all")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+}
