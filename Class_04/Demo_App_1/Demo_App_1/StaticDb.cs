d85bb0120c5f8fe1adad83858c57817d717dc609
Viktor Mileski <145224964+viksamiles77@users.noreply.github.com>
2024-06-10 20:53:57 +0200

    Class_04 / Views

---
+++ Class_04/Demo_App_1/Demo_App_1/StaticDb.cs
@@ -0,0 +1,23 @@
+﻿using ViewModels;
+
+namespace Demo_App_1
+{
+     public static class StaticDb
+    {
+        public static List<UserViewModel> Users = new List<UserViewModel>()
+        {
+            new UserViewModel
+            {
+                Id = 1,
+                FirstName = "Risto",
+                LastName = "Panchevski"
+            },
+            new UserViewModel
+            {
+                Id = 2,
+                FirstName = "Slave",
+                LastName = "Ivanovski"
+            },
+        };
+    }
+}
