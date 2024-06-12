7fd8ebbe7e4aee9980b47105da1d337b388e902d
Viktor Mileski <145224964+viksamiles77@users.noreply.github.com>
2024-06-12 17:55:12 +0200

    class 04

--- Class_04/Demo_App_1/Demo_App_1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Demo_App_1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
